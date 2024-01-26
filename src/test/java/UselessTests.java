18c9e499e8eeb29426c12aade8f8e81e1ce6b09c
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-01-26 16:48:04 +0100

    Update UselessTests.java

--- src/test/java/UselessTests.java
+++ src/test/java/UselessTests.java
@@ -4,9 +4,18 @@ import org.junit.jupiter.api.Test;
 public class UselessTests {
 
   @Test
-  public void testTest() {
+  public void testAssertionsEquals() {
     int result = Math.max(1, 10);
 
     Assertions.assertEquals(10, result);
   }
+
+  @Test
+  public void testAssertionsThrows() {
+    // this has to throw specified exception to pass the test
+    Assertions.assertThrows(ArithmeticException.class, () -> {
+      int result = 100 / 0;
+    });
+  }
+
 }
