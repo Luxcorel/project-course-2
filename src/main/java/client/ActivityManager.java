11306e9984a6b9d1764dee2944f531022537bc1a
edvintopa <55089202+edvintopa@users.noreply.github.com>
2024-02-02 19:51:14 +0100

    ActivityRegister class moved to client. Changed name to ActivityManager. New mechanisms to retrieve activites, one random or one specific (by new ID attribute) instead of old way of sending entire list. Replaced LinkedList with ArrayList to enable shuffle. Added createActivity method.

---
+++ src/main/java/client/ActivityManager.java
@@ -0,0 +1,95 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * This class creates a register that handles Activity objects.
+ *
+ * @author Carolin Nordstrom, Oscar Kareld, Chanon Borgstrom, Sofia Hallberg, Edvin Topalovic.
+ * @version 1.1
+ */
+
+public class ActivityManager {
+
+  private ArrayList<Activity> activityRegister;
+  public ActivityManager(String file) {
+    readRegister(file);
+  }
+
+  private void readRegister(String file) {
+    activityRegister = new ArrayList<>();
+    int nbrOfActivities;
+
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+      nbrOfActivities = Integer.parseInt(br.readLine());
+      for (int i = 0; i < nbrOfActivities; i++) {
+
+        Activity activity = new Activity();
+
+        activity.setActivityID(Integer.parseInt(br.readLine()));
+        activity.setActivityName(br.readLine());
+        activity.setActivityInstruction(br.readLine());
+        activity.setActivityInfo(br.readLine());
+        activity.createActivityImage(br.readLine());
+
+        activityRegister.add(activity);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+//  public LinkedList<Activity> getActivityRegister() {
+//    return activityRegister;
+//  }
+
+  /**
+   * Method returns random {@link Activity}-object from {@link #activityRegister}
+   * Randomization is achieved by shuffling the list.
+   * @author Edvin Topalovic
+   * @return {@link Activity}-object
+   */
+  public Activity getActivity() {
+    Collections.shuffle(activityRegister);
+    return activityRegister.get(0);
+  }
+
+  /**
+   * Method returns {@link Activity}-object by ID from {@link #activityRegister}
+   * @author Edvin Topalovic
+   * @param id
+   * @return {@link Activity}-object
+   */
+  public Activity getActivity(int id) {
+    for (Activity activity: activityRegister) {
+      if (activity.getActivityID() == id) { return activity; }
+    }
+    return null;
+  }
+
+  /**
+   * Method allows user to add custom activities to {@link #activityRegister}
+   * @author Edvin Topalovic
+   * @param name, inst, info
+   * @return {@link Activity}-object
+   */
+  public Activity createActivity(String name, String inst, String info) {
+    Activity activity = new Activity();
+    int newId = activityRegister.size();
+
+    activity.setActivityID(newId);
+    activity.setActivityName(name);
+    activity.setActivityInstruction(inst);
+    activity.setActivityInfo(info);
+    //activity.createActivityImage();
+
+    activityRegister.add(activity);
+
+    return getActivity(newId);
+  }
+}
