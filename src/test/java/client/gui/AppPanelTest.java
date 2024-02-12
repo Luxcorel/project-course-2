706a035aad8c3c761ed624698c03822e5552c219
samuelcarlsson02 <samuel.carlsson4@gmail.com>
2024-02-12 14:04:07 +0100

    created unit test for F010a start an interval with button

---
+++ src/test/java/client/gui/AppPanelTest.java
@@ -0,0 +1,34 @@
+package client.gui;
+
+import client.ClientController;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppPanelTest {
+  @Test
+  void testBtnStartTimerClick() throws IllegalAccessException, NoSuchFieldException, InterruptedException {
+    MainPanel mainPanelTest = new MainPanel(new MainFrame(new ClientController()), "Test");
+    AppPanel appPanelTest = new AppPanel(mainPanelTest);
+
+    Field fieldBtn = AppPanel.class.getDeclaredField("btnStartTimer");
+    fieldBtn.setAccessible(true);
+    JButton btnStartTimer = (JButton) fieldBtn.get(appPanelTest);
+
+    Field fieldLbl = AppPanel.class.getDeclaredField("lblTimerInfo");
+    fieldLbl.setAccessible(true);
+    JLabel lblTimerInfo = (JLabel) fieldLbl.get(appPanelTest);
+
+    String initialText = lblTimerInfo.getText();
+    btnStartTimer.doClick();
+    Thread.sleep(1000);
+    String afterClickText = lblTimerInfo.getText();
+
+    assertNotEquals(initialText, afterClickText);
+  }
+
+}
