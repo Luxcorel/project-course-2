5c0e9a63e3c051b6a7ccc89cc259ceabae54c617
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-02-18 18:36:34 +0100

    Removed server dependencies & refactoring of client
    *General changes*
    Removed all server dependencies from the client. This breaks some of the semantics of the client code as it won't serve the same purpose as it did before. For example: logout() is now only used to close the application.
    
    *Timer refactor*
    Refactored timer to use a timer based on seconds instead of minutes and seconds. This results in a lot less complexity of the code while achieving the same results.
    
    *Postponed activity & ActivityManager*
    Implemented ability to postpone activity when pressing the postpone button in the notification. To achieve this, a queue is added to ActivityManager. This queue consists of the postponed activities. As such getActivity() will remove an item from this queue if it is not empty - else it will get a random activity as usual.
    
    *GUI changes*
    Removed the change timer button as it introduced a couple of bugs when using the application. There's now only a start timer button which will kill all existing timers and start a new one with the specified timer interval. This might be changed in the future if the team considers this design to be not be good UX.

--- src/main/java/client/ActivityManager.java
+++ src/main/java/client/ActivityManager.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * This class creates a register that handles Activity objects.
@@ -17,8 +19,11 @@ import java.util.Collections;
 public class ActivityManager {
 
   private ArrayList<Activity> activityRegister;
+  private final Queue<Activity> postponedActivities;
+
   public ActivityManager(String file) {
     readRegister(file);
+    postponedActivities = new LinkedList<>();
   }
 
   private void readRegister(String file) {
@@ -51,32 +56,41 @@ public class ActivityManager {
   /**
    * Method returns random {@link Activity}-object from {@link #activityRegister}
    * Randomization is achieved by shuffling the list.
-   * @author Edvin Topalovic
+   *
    * @return {@link Activity}-object
+   * @author Edvin Topalovic
    */
   public Activity getActivity() {
+    if (!postponedActivities.isEmpty()) {
+      return postponedActivities.remove();
+    }
+
     Collections.shuffle(activityRegister);
-    return activityRegister.get(0);
+    return activityRegister.getFirst();
   }
 
   /**
    * Method returns {@link Activity}-object by ID from {@link #activityRegister}
-   * @author Edvin Topalovic
+   *
    * @param id
    * @return {@link Activity}-object
+   * @author Edvin Topalovic
    */
   public Activity getActivity(int id) {
-    for (Activity activity: activityRegister) {
-      if (activity.getActivityID() == id) { return activity; }
+    for (Activity activity : activityRegister) {
+      if (activity.getActivityID() == id) {
+        return activity;
+      }
     }
     return null;
   }
 
   /**
    * Method allows user to add custom activities to {@link #activityRegister}
-   * @author Edvin Topalovic
+   *
    * @param name, inst, info
    * @return {@link Activity}-object
+   * @author Edvin Topalovic
    */
   public Activity createActivity(String name, String inst, String info) {
     Activity activity = new Activity();
@@ -92,4 +106,13 @@ public class ActivityManager {
 
     return getActivity(newId);
   }
+
+  public void enqueueActivity(Activity activity) {
+    postponedActivities.add(activity);
+  }
+
+  public Queue<Activity> getPostponedActivities() {
+    return postponedActivities;
+  }
+
 }
