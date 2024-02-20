78af09eb550980f58588e943ddd18f1501066c91
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-02-20 23:08:07 +0100

    Finished the implementation of saving of activities to disk
    *Moved to JSON-based serialisation*
    The old method of saving/reading from disk was fragile and undocumented. As such I made the decision to use a more standardised approach by using a JSON serialisation library, in this case Gson. It works by saving objects to JSON format and then when reading it builds Java objects from the JSON. This approach has numerus benefits. The first being that the data is human readable as it contains a key, e.g. "activityName" and a value, e.g. "Situps". The second reason is that this approach supports easily adding new attributes in the future to the activity class without breaking the reading/writing methods.
    
    *Error handling*
    Improved error handling when reading/writing files. Also added suitable error messages that also points to the current method to aid in locating potential errors.
    
    *Miscellaneous changes*
    Added the helper methods fileExists() and createFile() to make the code more readable. Also added wrapper methods for save/load activities that uses the already set up activitiesFilePath in ActivityManager.

--- src/main/java/client/ActivityManager.java
+++ src/main/java/client/ActivityManager.java
@@ -1,75 +1,111 @@
 package client;
 
-import java.io.BufferedReader;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.UUID;
 
 /**
  * This class creates a register that handles Activity objects.
  *
  * @author Carolin Nordstrom, Oscar Kareld, Chanon Borgstrom, Sofia Hallberg, Edvin Topalovic.
- * @version 1.1.2
+ * @author Johannes Rosengren
  */
-
 public class ActivityManager {
+  private final String activitiesFilePath;
+  private final ArrayList<Activity> activityRegister;
 
-  private ArrayList<Activity> activityRegister;
-  public ActivityManager(String file) {
-    readRegister(file);
+  /**
+   * The specified file path will be used to read and write activities to and from a file.
+   *
+   * @param activitiesFilePath The activity save-file to use.
+   */
+  public ActivityManager(String activitiesFilePath) {
+    this.activitiesFilePath = activitiesFilePath;
+
+    this.activityRegister = readActivitiesFromDisc(activitiesFilePath);
   }
 
-  private void readRegister(String file) {
-    activityRegister = new ArrayList<>();
-    int nbrOfActivities;
+  /**
+   * Checks if a file exists.
+   *
+   * @param filePath The file path to check.
+   * @return true if the file exists, otherwise false.
+   * @author Johannes Rosengren
+   */
+  private boolean fileExists(String filePath) {
+    return new File(filePath).exists();
+  }
 
-    try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
-      String line = br.readLine();
-      if (line != null) {
-        nbrOfActivities = Integer.parseInt(line);
-        for (int i = 0; i < nbrOfActivities; i++) {
-          Activity activity = new Activity();
-
-          // Read each attribute separately
-          String idString = br.readLine();
-          String name = br.readLine();
-          String instruction = br.readLine();
-          String info = br.readLine();
-          String imageDescription = br.readLine();
-
-          // Check for null values before parsing
-          if (idString != null) {
-            activity.setActivityID(Integer.parseInt(idString));
-          }
-          if (name != null) {
-            activity.setActivityName(name);
-          }
-          if (instruction != null) {
-            activity.setActivityInstruction(instruction);
-          }
-          if (info != null) {
-            activity.setActivityInfo(info);
-          }
-          if (imageDescription != null) {
-            activity.createActivityImage(imageDescription);
-          }
-
-          activityRegister.add(activity);
-        }
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+  /**
+   * Creates an empty file at the specified file path if no file exists there already.
+   *
+   * @param filePath Where to create the file.
+   * @throws IOException if the file could not be created.
+   * @author Johannes Rosengren
+   */
+  private void createFile(String filePath) throws IOException {
+    File file = new File(filePath);
+    if (!file.exists()) {
+      file.createNewFile();
     }
   }
 
+  /**
+   * Reads an activity save-file and returns an ArrayList of Activity objects.
+   * If the file does not exist or is empty, an empty ArrayList is returned.
+   *
+   * @param filePath The file to read from
+   * @return An ArrayList of {@link Activity} objects
+   * @throws RuntimeException if the file could not be read
+   * @author Johannes Rosengren
+   */
+  public ArrayList<Activity> readActivitiesFromDisc(String filePath) {
+    if (!fileExists(filePath)) {
+      return new ArrayList<>();
+    }
 
-//  public LinkedList<Activity> getActivityRegister() {
-//    return activityRegister;
-//  }
+    File activitiesFile = new File(filePath);
+    // If an Activity save-file exists, read it and return it as an Activity Arraylist
+    try (FileReader reader = new FileReader(activitiesFile, StandardCharsets.UTF_8)) {
+      Gson gson = new Gson();
+      ArrayList<Activity> activities = gson.fromJson(reader, new TypeToken<ArrayList<Activity>>() {
+      }.getType());
+      if (activities == null) {
+        return new ArrayList<>();
+      }
+
+      // Java Swing ImageIcons can't be serialized,
+      // so we have to reinitialize all the ImageIcons manually
+      for (Activity activity : activities) {
+        activity.setActivityImage(activity.getImagePath());
+      }
+
+      return activities;
+    } catch (IOException e) {
+      throw new RuntimeException(
+          "ActivityManager.readActivitiesFromDisc(): Could not read activity file");
+    }
+  }
+
+  /**
+   * Reads the activity save-file and returns an ArrayList of Activity objects.
+   * If the file does not exist or is empty, an empty ArrayList is returned.
+   *
+   * @return An ArrayList of {@link Activity} objects
+   * @throws RuntimeException if the file could not be read
+   * @author Johannes Rosengren
+   */
+  public ArrayList<Activity> readActivitiesFromDisc() {
+    return readActivitiesFromDisc(activitiesFilePath);
+  }
 
   /**
    * Method returns random {@link Activity}-object from {@link #activityRegister}
@@ -84,63 +120,111 @@ public class ActivityManager {
 
   /**
    * Method returns {@link Activity}-object by ID from {@link #activityRegister}
-   * @author Edvin Topalovic
-   * @param id
+   *
+   * @param id ID of the activity
    * @return {@link Activity}-object
+   * @author Edvin Topalovic
    */
-  public Activity getActivity(int id) {
-    for (Activity activity: activityRegister) {
-      if (activity.getActivityID() == id) { return activity; }
+  public Activity getActivity(String id) {
+    for (Activity activity : activityRegister) {
+      if (activity.getActivityID().equals(id)) {
+        return activity;
+      }
     }
     return null;
   }
 
   /**
    * Method allows user to add custom activities to {@link #activityRegister}
+   *
+   * @param activityName        Name of the activity
+   * @param activityInstruction Instructions for the activity
+   * @param activityInfo        Information about the activity
+   * @return the added {@link Activity} object
    * @author Edvin Topalovic
-   * @param name, inst, info
-   * @return {@link Activity}-object
    */
-  public Activity createActivity(String name, String inst, String info) {
-    Activity activity = new Activity();
-    int newId = activityRegister.size() + 1;
-
-    activity.setActivityID(newId);
-    activity.setActivityName(name);
-    activity.setActivityInstruction(inst);
-    activity.setActivityInfo(info);
-    //activity.createActivityImage();
-
-    activityRegister.add(activity);
-
-    return getActivity(newId);
+  public Activity createActivity(String activityName, String activityInstruction,
+      String activityInfo) {
+    return createActivity(activityName, activityInstruction, activityInfo, null);
   }
 
   /**
-   * Saves activites to ./files/activities.txt
+   * Method allows user to add custom activities to {@link #activityRegister}
+   *
+   * @param activityName        Name of the activity
+   * @param activityInstruction Instructions for the activity
+   * @param activityInfo        Information about the activity
+   * @param imagePath           Path to the image for the activity
+   * @return the added {@link Activity} object
    * @author Edvin Topalovic
+   * @author Johannes Rosengren
    */
-  public void saveActivitiesToDisc(String file) {
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+  public Activity createActivity(String activityName, String activityInstruction,
+      String activityInfo, String imagePath) {
+    Activity activity = new Activity();
 
-      writer.write(String.valueOf(activityRegister.size()));  // amount of objects
-      writer.newLine();
+    activity.setActivityID(UUID.randomUUID().toString());
+    activity.setActivityName(activityName);
+    activity.setActivityInstruction(activityInstruction);
+    activity.setActivityInfo(activityInfo);
+    activity.setActivityImage(imagePath);
 
-      for (Activity activity : activityRegister) {
-        writer.write(activity.getActivityID() + "");
-        writer.newLine();
-        writer.write(activity.getActivityName());
-        writer.newLine();
-        writer.write(activity.getActivityInstruction());
-        writer.newLine();
-        writer.write(activity.getActivityInfo());
-        writer.newLine();
-        writer.write(activity.getActivityImage().getDescription());
-        writer.newLine();
+    activityRegister.add(activity);
+    saveActivitiesToDisc(activitiesFilePath);
+
+    return getActivity(activity.getActivityID());
+  }
+
+  /**
+   * Saves activities to specified file.
+   * If the file does not already exist, it will be created.
+   *
+   * @param filePath The file to save activities to
+   * @throws RuntimeException if the file could not be created or written to
+   * @author Edvin Topalovic
+   * @author Johannes Rosengren
+   */
+  public void saveActivitiesToDisc(String filePath) {
+    Gson gson = new Gson();
+    String encodedActivities = gson.toJson(activityRegister);
+
+    if (!fileExists(filePath)) {
+      try {
+        createFile(filePath);
+      } catch (IOException e) {
+        throw new RuntimeException(
+            "ActivityManager.saveActivitiesToDisc(): Could not create activity save-file");
       }
+    }
 
+    File activitiesFile = new File(filePath);
+    try (BufferedWriter writer = new BufferedWriter(
+        new FileWriter(activitiesFile, StandardCharsets.UTF_8))) {
+      writer.write(encodedActivities);
     } catch (IOException e) {
-        throw new RuntimeException(e);
+      throw new RuntimeException(
+          "ActivityManager.saveActivitiesToDisc(): Could not write to activity save-file");
     }
   }
+
+  /**
+   * Saves activities to the activity save-file.
+   * If the file does not already exist, it will be created.
+   *
+   * @throws RuntimeException if the file could not be created or written to
+   * @author Edvin Topalovic
+   * @author Johannes Rosengren
+   */
+  public void saveActivitiesToDisc() {
+    saveActivitiesToDisc(activitiesFilePath);
+  }
+
+  public ArrayList<Activity> getActivityRegister() {
+    return activityRegister;
+  }
+
+  public String getActivitiesFilePath() {
+    return activitiesFilePath;
+  }
+
 }
