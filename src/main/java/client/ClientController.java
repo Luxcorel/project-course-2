d015d1a33024088a2bcfbb64494e5599fad2e9fc
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-02-24 05:57:34 +0100

    Added addActivity methods in ClientController
    Also added the variable "programDataPath" to be able to configure where to save user data files

--- src/main/java/client/ClientController.java
+++ src/main/java/client/ClientController.java
@@ -6,33 +6,58 @@ import client.gui.MainFrame;
  * This class manages the logic for the Client and controls the data flow.
  *
  * @author Carolin Nordstrom, Oscar Kareld, Chanon Borgstrom, Sofia Hallberg.
+ * @author Johannes Rosengren
  * @version 1.0
  */
 
 public class ClientController {
 
+  private final String programDataPath;
   private MainFrame mainFrame;
-  private ActivityManager activityManager;
+  private final ActivityManager activityManager;
 
   /**
-   * Constructs a MainFrame and a ClientCommunicationController object. Then calls the method
-   * createUser.
+   * Constructor for the ClientController.
+   *
+   * @param programDataFolder The path to the program data folder excluding "/". Example: "files"
    */
-  public ClientController() {
-    mainFrame = new MainFrame(this);
+  public ClientController(String programDataFolder) {
+    this.programDataPath = programDataFolder;
     activityManager = new ActivityManager(
-        "files/activities.txt"); // TODO: environment variable needed instead of this local one!
+        programDataFolder + "/activities.json");
+    mainFrame = new MainFrame(this);
   }
 
-  /**
-   * Sets the UserType to LOGOUT and sends the user object to ClientCommunicationController.
-   */
   public void logOut() {
     System.exit(0);
   }
 
   public ActivityManager getActivityManager() {
     return activityManager;
+  }
+
+  /**
+   * @param activityName        The name of the activity.
+   * @param activityInstruction The instruction for the activity.
+   * @param activityInfo        The information for the activity.
+   * @return The {@link Activity} that was added.
+   */
+  public Activity addActivity(String activityName, String activityInstruction,
+      String activityInfo) {
+    return activityManager.createActivity(activityName, activityInstruction, activityInfo);
+  }
+
+  /**
+   * @param activityName        The name of the activity.
+   * @param activityInstruction The instruction for the activity.
+   * @param activityInfo        The information for the activity.
+   * @param imagePath           The path to the image for the activity.
+   * @return The {@link Activity} that was added.
+   */
+  public Activity addActivity(String activityName, String activityInstruction,
+      String activityInfo, String imagePath) {
+    return activityManager.createActivity(activityName, activityInstruction, activityInfo,
+        imagePath);
   }
 
 }
