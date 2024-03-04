217a535b3d763b3d9726a2bf4ee0beeede511350
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-03-04 20:23:35 +0100

    Make platform specific tests run only on target platforms

--- src/test/java/client/OSDetectionTests.java
+++ src/test/java/client/OSDetectionTests.java
@@ -1,40 +1,44 @@
 package client;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import client.OSDetection.OS;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
 
 /**
  * @author edvintopa
- * @project project-course-2
- * @created 2024-02-20
+ * @author Johannes Rosengren
  */
-class OSDetectionTest {
+public class OSDetectionTests {
 
-  /*
-   * ONLY ONE OF THESE ARE SUPPOSED TO PASS
-   */
   @Test
-  void getOS_Mac() {
+  @EnabledOnOs(org.junit.jupiter.api.condition.OS.MAC)
+  public void getOS_Mac() {
     OSDetection.OS expectedOS = OS.MAC;
     OSDetection.OS actualOS = OSDetection.getOS();
 
     assertEquals(expectedOS, actualOS);
   }
+
   @Test
-  void getOS_Windows() {
+  @EnabledOnOs(org.junit.jupiter.api.condition.OS.WINDOWS)
+  public void getOS_Windows() {
     OSDetection.OS expectedOS = OS.WINDOWS;
     OSDetection.OS actualOS = OSDetection.getOS();
 
     assertEquals(expectedOS, actualOS);
   }
-
+  
   @Test
-  void getOS_Unsupported() {
+  @DisabledOnOs({org.junit.jupiter.api.condition.OS.MAC,
+      org.junit.jupiter.api.condition.OS.WINDOWS})
+  public void getOS_Unsupported() {
     OSDetection.OS expectedOS = OS.UNSUPPORTED;
     OSDetection.OS actualOS = OSDetection.getOS();
 
     assertEquals(expectedOS, actualOS);
   }
+
 }
