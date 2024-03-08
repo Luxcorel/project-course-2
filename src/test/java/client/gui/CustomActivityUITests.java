ebc7b0918b31f81481cf6c48e001fa9ab93af435
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-03-09 00:48:04 +0100

    Add tests targeting F011 (CustomActivityUI.getActivity())

--- src/test/java/client/gui/CustomActivityUITests.java
+++ src/test/java/client/gui/CustomActivityUITests.java
@@ -581,4 +581,565 @@ public class CustomActivityUITests {
     assertEquals(expected, added);
   }
 
+  // CustomActivityUI.getActivity() cases
+
+  // ACTIVITY SHOULD NOT BE RETURNED CASES
+
+  /**
+   * Test case ID: TC-34.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_submitCustomActivityWithEmptyStrings_shouldFail() {
+    ClientController mock = mock(ClientController.class);
+
+    when(mock.packageActivity("", "", ""))
+        .thenThrow(new AssertionError("Should not have tried to add incomplete activity"));
+
+    IMessageProvider messageProvider = new IMessageProvider() {
+      @Override
+      public int showConfirmDialog(Component parentComponent, Object message, String title,
+          int optionType, int messageType) {
+        return JOptionPane.OK_OPTION;
+      }
+
+      @Override
+      public void showMessageDialog(Component parentComponent, Object message, String title,
+          int messageType) {
+        if (messageType == JOptionPane.ERROR_MESSAGE) {
+          throw new IllegalArgumentException("Incomplete activity was blocked from being added");
+        }
+      }
+
+    };
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+
+    assertThrows(IllegalArgumentException.class, ui::getCustomActivity);
+  }
+
+  /**
+   * Test case ID: TC-35.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_submitCustomActivityWithNulls_shouldFail() {
+    ClientController mock = mock(ClientController.class);
+
+    when(mock.packageActivity(null, null, null))
+        .thenThrow(new AssertionError("Should not have tried to add incomplete activity"));
+
+    IMessageProvider messageProvider = new IMessageProvider() {
+      @Override
+      public int showConfirmDialog(Component parentComponent, Object message, String title,
+          int optionType, int messageType) {
+        return JOptionPane.OK_OPTION;
+      }
+
+      @Override
+      public void showMessageDialog(Component parentComponent, Object message, String title,
+          int messageType) {
+        if (messageType == JOptionPane.ERROR_MESSAGE) {
+          throw new IllegalArgumentException("Incomplete activity was blocked from being added");
+        }
+      }
+
+    };
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+    ui.setActivityName(null);
+    ui.setActivityInstruction(null);
+    ui.setActivityInfo(null);
+    ui.setImage(null);
+
+    assertThrows(IllegalArgumentException.class, ui::getCustomActivity);
+  }
+
+  /**
+   * Test case ID: TC-36.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_submitCustomActivityWithNameButOthersEmptyString_shouldFail() {
+    ClientController mock = mock(ClientController.class);
+
+    when(mock.packageActivity("Test name", "", ""))
+        .thenThrow(new AssertionError("Should not have tried to add incomplete activity"));
+
+    IMessageProvider messageProvider = new IMessageProvider() {
+      @Override
+      public int showConfirmDialog(Component parentComponent, Object message, String title,
+          int optionType, int messageType) {
+        return JOptionPane.OK_OPTION;
+      }
+
+      @Override
+      public void showMessageDialog(Component parentComponent, Object message, String title,
+          int messageType) {
+        if (messageType == JOptionPane.ERROR_MESSAGE) {
+          throw new IllegalArgumentException("Incomplete activity was blocked from being added");
+        }
+      }
+
+    };
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+    ui.setActivityName("Test name");
+
+    assertThrows(IllegalArgumentException.class, ui::getCustomActivity);
+  }
+
+  /**
+   * Test case ID: TC-37.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_submitCustomActivityWithNameAndInfoButOthersEmptyString_shouldFail() {
+    ClientController mock = mock(ClientController.class);
+
+    when(mock.packageActivity("Test name", "", "Test info"))
+        .thenThrow(new AssertionError("Should not have tried to add incomplete activity"));
+
+    IMessageProvider messageProvider = new IMessageProvider() {
+      @Override
+      public int showConfirmDialog(Component parentComponent, Object message, String title,
+          int optionType, int messageType) {
+        return JOptionPane.OK_OPTION;
+      }
+
+      @Override
+      public void showMessageDialog(Component parentComponent, Object message, String title,
+          int messageType) {
+        if (messageType == JOptionPane.ERROR_MESSAGE) {
+          throw new IllegalArgumentException("Incomplete activity was blocked from being added");
+        }
+      }
+
+    };
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+    ui.setActivityName("Test name");
+    ui.setImage("Test info");
+
+    assertThrows(IllegalArgumentException.class, ui::getCustomActivity);
+  }
+
+  /**
+   * Test case ID: TC-38.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_submitCustomActivityWithNameAndInstructionButOthersEmptyString_shouldFail() {
+    ClientController mock = mock(ClientController.class);
+
+    when(mock.packageActivity("Test name", "Test instruction", ""))
+        .thenThrow(new AssertionError("Should not have tried to add incomplete activity"));
+
+    IMessageProvider messageProvider = new IMessageProvider() {
+      @Override
+      public int showConfirmDialog(Component parentComponent, Object message, String title,
+          int optionType, int messageType) {
+        return JOptionPane.OK_OPTION;
+      }
+
+      @Override
+      public void showMessageDialog(Component parentComponent, Object message, String title,
+          int messageType) {
+        if (messageType == JOptionPane.ERROR_MESSAGE) {
+          throw new IllegalArgumentException("Incomplete activity was blocked from being added");
+        }
+      }
+
+    };
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+    ui.setActivityName("Test name");
+    ui.setActivityInstruction("Test instruction");
+
+    assertThrows(IllegalArgumentException.class, ui::getCustomActivity);
+  }
+
+  /**
+   * Test case ID: TC-39.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_submitCustomActivityWithInstructionButOthersEmptyString_shouldFail() {
+    ClientController mock = mock(ClientController.class);
+
+    when(mock.packageActivity("", "Test instruction", ""))
+        .thenThrow(new AssertionError("Should not have tried to add incomplete activity"));
+
+    IMessageProvider messageProvider = new IMessageProvider() {
+      @Override
+      public int showConfirmDialog(Component parentComponent, Object message, String title,
+          int optionType, int messageType) {
+        return JOptionPane.OK_OPTION;
+      }
+
+      @Override
+      public void showMessageDialog(Component parentComponent, Object message, String title,
+          int messageType) {
+        if (messageType == JOptionPane.ERROR_MESSAGE) {
+          throw new IllegalArgumentException("Incomplete activity was blocked from being added");
+        }
+      }
+
+    };
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+    ui.setActivityInstruction("Test instruction");
+
+    assertThrows(IllegalArgumentException.class, ui::getCustomActivity);
+  }
+
+  /**
+   * Test case ID: TC-40.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_submitCustomActivityWithInstructionAndInfoButOthersEmptyString_shouldFail() {
+    ClientController mock = mock(ClientController.class);
+
+    when(mock.packageActivity("", "Test instruction", "Test info"))
+        .thenThrow(new AssertionError("Should not have tried to add incomplete activity"));
+
+    IMessageProvider messageProvider = new IMessageProvider() {
+      @Override
+      public int showConfirmDialog(Component parentComponent, Object message, String title,
+          int optionType, int messageType) {
+        return JOptionPane.OK_OPTION;
+      }
+
+      @Override
+      public void showMessageDialog(Component parentComponent, Object message, String title,
+          int messageType) {
+        if (messageType == JOptionPane.ERROR_MESSAGE) {
+          throw new IllegalArgumentException("Incomplete activity was blocked from being added");
+        }
+      }
+
+    };
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+    ui.setActivityInstruction("Test instruction");
+    ui.setActivityInfo("Test info");
+
+    assertThrows(IllegalArgumentException.class, ui::getCustomActivity);
+  }
+
+  /**
+   * Test case ID: TC-41.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_submitCustomActivityWithInfoButOthersEmptyString_shouldFail() {
+    ClientController mock = mock(ClientController.class);
+
+    when(mock.packageActivity("", "", "Test info"))
+        .thenThrow(new AssertionError("Should not have tried to add incomplete activity"));
+
+    IMessageProvider messageProvider = new IMessageProvider() {
+      @Override
+      public int showConfirmDialog(Component parentComponent, Object message, String title,
+          int optionType, int messageType) {
+        return JOptionPane.OK_OPTION;
+      }
+
+      @Override
+      public void showMessageDialog(Component parentComponent, Object message, String title,
+          int messageType) {
+        if (messageType == JOptionPane.ERROR_MESSAGE) {
+          throw new IllegalArgumentException("Incomplete activity was blocked from being added");
+        }
+      }
+
+    };
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+    ui.setActivityInfo("Test info");
+
+    assertThrows(IllegalArgumentException.class, ui::getCustomActivity);
+  }
+
+  /**
+   * Test case ID: TC-42.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_enterCustomActivityWithEmptyStringsButCancel_shouldNotReturnActivity() {
+    ClientController mock = mock(ClientController.class);
+
+    when(mock.packageActivity("", "", ""))
+        .thenThrow(new AssertionError("Should not have tried to add incomplete activity"));
+
+    IMessageProvider messageProvider = new IMessageProvider() {
+      @Override
+      public int showConfirmDialog(Component parentComponent, Object message, String title,
+          int optionType, int messageType) {
+        return JOptionPane.CANCEL_OPTION;
+      }
+
+      @Override
+      public void showMessageDialog(Component parentComponent, Object message, String title,
+          int messageType) {
+        fail("Should not have encountered error message");
+      }
+
+    };
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+
+    assertTrue(ui.getCustomActivity().isEmpty());
+  }
+
+  /**
+   * Test case ID: TC-43.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_enterCustomActivityWithNameAndDescriptionAndInfoAndImageButCancel_shouldNotReturnActivity() {
+    ClientController mock = mock(ClientController.class);
+
+    when(mock.packageActivity("Test name", "Test instruction", "Test info",
+        "src/test/resources/test_image.png"))
+        .thenThrow(new AssertionError("Should not have added activity on cancel"));
+
+    IMessageProvider messageProvider = new IMessageProvider() {
+      @Override
+      public int showConfirmDialog(Component parentComponent, Object message, String title,
+          int optionType, int messageType) {
+        return JOptionPane.CANCEL_OPTION;
+      }
+
+      @Override
+      public void showMessageDialog(Component parentComponent, Object message, String title,
+          int messageType) {
+        if (messageType == JOptionPane.ERROR_MESSAGE) {
+          fail("Shouldn't have encountered error message");
+        }
+      }
+
+    };
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+    ui.setActivityName("Test name");
+    ui.setActivityInstruction("Test instruction");
+    ui.setActivityInfo("Test info");
+    ui.setImage("src/test/resources/test_image.png");
+
+    assertTrue(ui.getCustomActivity().isEmpty());
+  }
+
+  /**
+   * Test case ID: TC-44.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_submitCustomActivityWithInvalidInputThenCancel_shouldNotReturnActivity() {
+    ClientController mock = mock(ClientController.class);
+
+    when(mock.packageActivity("Test name", "Test instruction", "Test info",
+        "src/test/resources/test_image.png"))
+        .thenThrow(new AssertionError("Should not have added activity on cancel"));
+
+    IMessageProvider messageProvider = Mockito.mock(IMessageProvider.class);
+    when(messageProvider.showConfirmDialog(any(), any(Object.class), anyString(), anyInt(),
+        anyInt())).thenReturn(
+        JOptionPane.OK_OPTION, JOptionPane.CANCEL_OPTION);
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+
+    assertTrue(ui.getCustomActivity().isEmpty());
+  }
+
+  // ACTIVITY SHOULD BE RETURNED CASES
+
+  /**
+   * Test case ID: TC-45.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_submitCustomActivityWithNameAndDescriptionAndInfo_shouldReturnActivity() {
+    ClientController mock = mock(ClientController.class);
+
+    Activity expected = new Activity();
+    expected.setActivityName("Test name");
+    expected.setActivityInstruction("Test instruction");
+    expected.setActivityInfo("Test info");
+
+    when(mock.packageActivity("Test name", "Test instruction", "Test info"))
+        .thenReturn(expected);
+
+    IMessageProvider messageProvider = new IMessageProvider() {
+      @Override
+      public int showConfirmDialog(Component parentComponent, Object message, String title,
+          int optionType, int messageType) {
+        return JOptionPane.OK_OPTION;
+      }
+
+      @Override
+      public void showMessageDialog(Component parentComponent, Object message, String title,
+          int messageType) {
+        if (messageType == JOptionPane.ERROR_MESSAGE) {
+          fail("Should not have encountered error message");
+        }
+      }
+
+    };
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+    ui.setActivityName("Test name");
+    ui.setActivityInstruction("Test instruction");
+    ui.setActivityInfo("Test info");
+
+    Activity added = ui.getCustomActivity().orElseThrow();
+    assertEquals(expected, added);
+  }
+
+  /**
+   * Test case ID: TC-46.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_submitCustomActivityWithNameAndDescriptionAndInfoAndImage_shouldReturnActivity() {
+    ClientController mock = mock(ClientController.class);
+
+    Activity expected = new Activity();
+    expected.setActivityName("Test name");
+    expected.setActivityInstruction("Test instruction");
+    expected.setActivityInfo("Test info");
+    expected.setActivityImage("src/test/resources/test_image.png");
+
+    when(mock.packageActivity("Test name", "Test instruction", "Test info",
+        "src/test/resources/test_image.png"))
+        .thenReturn(expected);
+
+    IMessageProvider messageProvider = new IMessageProvider() {
+      @Override
+      public int showConfirmDialog(Component parentComponent, Object message, String title,
+          int optionType, int messageType) {
+        return JOptionPane.OK_OPTION;
+      }
+
+      @Override
+      public void showMessageDialog(Component parentComponent, Object message, String title,
+          int messageType) {
+        if (messageType == JOptionPane.ERROR_MESSAGE) {
+          fail("Should not have encountered error message");
+        }
+      }
+
+    };
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+    ui.setActivityName("Test name");
+    ui.setActivityInstruction("Test instruction");
+    ui.setActivityInfo("Test info");
+    ui.setImage("src/test/resources/test_image.png");
+
+    Activity added = ui.getCustomActivity().orElseThrow();
+    assertEquals(expected, added);
+  }
+
+  /**
+   * Test case ID: TC-47.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_submitCustomActivityWithNameAndDescriptionAndInfoAndImageButDeselectImage_shouldReturnActivityWithoutImage() {
+    ClientController mock = mock(ClientController.class);
+
+    Activity expected = new Activity();
+    expected.setActivityName("Test name");
+    expected.setActivityInstruction("Test instruction");
+    expected.setActivityInfo("Test info");
+
+    when(mock.packageActivity("Test name", "Test instruction", "Test info",
+        "src/test/resources/test_image.png"))
+        .thenThrow(new AssertionError("Should not have added with image"));
+
+    when(mock.packageActivity("Test name", "Test instruction", "Test info"))
+        .thenReturn(expected);
+
+    IMessageProvider messageProvider = new IMessageProvider() {
+      @Override
+      public int showConfirmDialog(Component parentComponent, Object message, String title,
+          int optionType, int messageType) {
+        return JOptionPane.OK_OPTION;
+      }
+
+      @Override
+      public void showMessageDialog(Component parentComponent, Object message, String title,
+          int messageType) {
+        if (messageType == JOptionPane.ERROR_MESSAGE) {
+          fail("Should not have encountered error message");
+        }
+      }
+
+    };
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+    ui.setActivityName("Test name");
+    ui.setActivityInstruction("Test instruction");
+    ui.setActivityInfo("Test info");
+    ui.setImage("src/test/resources/test_image.png");
+    ui.resetImage();
+
+    Activity added = ui.getCustomActivity().orElseThrow();
+    assertEquals(expected, added);
+  }
+
+  /**
+   * Test case ID: TC-48.
+   * Requirements: F011.
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void getActivity_beginCustomActivityWithInvalidInputsButThenEnterNameAndDescriptionAndInfoAndImage_shouldReturnActivity() {
+    ClientController mock = mock(ClientController.class);
+
+    Activity expected = new Activity();
+    expected.setActivityName("Test name");
+    expected.setActivityInstruction("Test instruction");
+    expected.setActivityInfo("Test info");
+    expected.setActivityImage("src/test/resources/test_image.png");
+
+    when(mock.packageActivity("Test name", "Test instruction", "Test info",
+        "src/test/resources/test_image.png"))
+        .thenReturn(expected);
+
+    IMessageProvider messageProvider = Mockito.mock(IMessageProvider.class);
+    when(messageProvider.showConfirmDialog(any(), any(Object.class), anyString(), anyInt(),
+        anyInt())).thenReturn(
+        JOptionPane.OK_OPTION);
+
+    CustomActivityUI ui = new CustomActivityUI(mock, messageProvider);
+
+    doAnswer(invocation -> {
+      ui.setActivityName("Test name");
+      ui.setActivityInstruction("Test instruction");
+      ui.setActivityInfo("Test info");
+      ui.setImage("src/test/resources/test_image.png");
+      return null;
+    }).when(messageProvider).showMessageDialog(any(), any(Object.class), anyString(), anyInt());
+
+    Activity added = ui.getCustomActivity().orElseThrow();
+    assertEquals(expected, added);
+  }
+
 }
