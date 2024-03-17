e3762e54b083b305f01acf4c3b8303653d2cc5de
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-03-17 01:51:01 +0100

    Add tests targeting requirement F010a

--- src/test/java/client/gui/AppPanelTests.java
+++ src/test/java/client/gui/AppPanelTests.java
@@ -1,16 +1,19 @@
 package client.gui;
 
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.after;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import client.ActivityManager;
+import client.Activity;
 import client.ClientController;
 import client.external.InspirationalQuotes;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import java.util.Optional;
 import java.util.Timer;
 import javax.swing.JDialog;
 import org.junit.jupiter.api.Test;
@@ -37,6 +40,93 @@ public class AppPanelTests {
     welcomeMessageUI.showWelcomeMessage();
 
     verify(dialogMock).setVisible(true);
+  }
+
+  /**
+   * Test case ID: TC-62.
+   * Requirements: F010a.
+   *
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void activityNotificationShouldShowAfterTimerReachesZero_TimerSetTo0Minutes() {
+    ClientController clientControllerMock = mock(ClientController.class);
+    MainPanel mainPanelMock = mock(MainPanel.class);
+    WelcomeMessageUI welcomeMessageMock = mock(WelcomeMessageUI.class);
+
+    AppPanel appPanel = spy(new AppPanel(mainPanelMock, clientControllerMock, welcomeMessageMock));
+
+    Activity randomActivity = new Activity();
+    randomActivity.setActivityName("Test activity");
+    randomActivity.setActivityInstruction("Test instruction");
+    randomActivity.setActivityInfo("Test info");
+    when(clientControllerMock.getActivity()).thenReturn(Optional.of(randomActivity));
+
+    doAnswer(args -> null)
+        .when(appPanel)
+        .showNotification(randomActivity);
+
+    appPanel.startTimer(0);
+
+    verify(appPanel, timeout(1000)).showNotification(randomActivity);
+  }
+
+  /**
+   * Test case ID: TC-63.
+   * Requirements: F010a.
+   *
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void activityNotificationShouldShowAfterTimerReachesZero_TimerSetTo1Minute() {
+    ClientController clientControllerMock = mock(ClientController.class);
+    MainPanel mainPanelMock = mock(MainPanel.class);
+    WelcomeMessageUI welcomeMessageMock = mock(WelcomeMessageUI.class);
+
+    AppPanel appPanel = spy(new AppPanel(mainPanelMock, clientControllerMock, welcomeMessageMock));
+
+    Activity randomActivity = new Activity();
+    randomActivity.setActivityName("Test activity");
+    randomActivity.setActivityInstruction("Test instruction");
+    randomActivity.setActivityInfo("Test info");
+    when(clientControllerMock.getActivity()).thenReturn(Optional.of(randomActivity));
+
+    doAnswer(args -> null)
+        .when(appPanel)
+        .showNotification(randomActivity);
+
+    appPanel.startTimer(1);
+
+    verify(appPanel, after(61_000)).showNotification(randomActivity);
+  }
+
+  /**
+   * Test case ID: TC-64.
+   * Requirements: F010a.
+   *
+   * @author Johannes Rosengren
+   */
+  @Test
+  public void activityNotificationShouldGrabRandomActivity() {
+    ClientController clientControllerMock = mock(ClientController.class);
+    MainPanel mainPanelMock = mock(MainPanel.class);
+    WelcomeMessageUI welcomeMessageMock = mock(WelcomeMessageUI.class);
+
+    AppPanel appPanel = spy(new AppPanel(mainPanelMock, clientControllerMock, welcomeMessageMock));
+
+    Activity randomActivity = new Activity();
+    randomActivity.setActivityName("Test activity");
+    randomActivity.setActivityInstruction("Test instruction");
+    randomActivity.setActivityInfo("Test info");
+    when(clientControllerMock.getActivity()).thenReturn(Optional.of(randomActivity));
+
+    doAnswer(args -> null)
+        .when(appPanel)
+        .showNotification(randomActivity);
+
+    appPanel.startTimer(0);
+
+    verify(clientControllerMock, timeout(1000)).getActivity();
   }
 
 }
